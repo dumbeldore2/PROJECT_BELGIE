5b9c60e86a96f3ee4b1006f84ddb5eade3f7d114
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-06-15 16:29:35 +0200

    er is een connectie tussen ma1 en ma 2 en alle namen van de textviews zijn ook veranderd en aangepast

--- app/src/main/java/com/example/project_belgie/MainActivity2.java
+++ app/src/main/java/com/example/project_belgie/MainActivity2.java
@@ -2,13 +2,36 @@ package com.example.project_belgie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    //textviews initen
+    TextView textView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        //textviews connecteren
+        textView1 = findViewById(R.id.text1);
+
+        //functies
+        click_back_1();
+
+    }
+
+    public void click_back_1(){
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

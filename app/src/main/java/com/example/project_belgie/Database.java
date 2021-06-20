c982079a0e2e2a5b4f95da10af3f1a6aaa51c599
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-06-21 00:35:03 +0200

    een database is aangemaakt met naam, nog niks anders werkt tho lol

--- app/src/main/java/com/example/project_belgie/Database.java
+++ app/src/main/java/com/example/project_belgie/Database.java
@@ -1,4 +1,29 @@
 package com.example.project_belgie;
 
-public class Database {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Database extends SQLiteOpenHelper {
+
+    //db naam
+    public static String DATABASE_NAME = "database_project_pp_mobile_app.db";
+
+
+
+    public Database(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }

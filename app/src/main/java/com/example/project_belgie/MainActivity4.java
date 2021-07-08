3966834a3a99bb56f2c55cc3e261521b825ce7e8
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-07-08 22:13:44 +0200

    textviews aangemaakt en back functie ook

--- app/src/main/java/com/example/project_belgie/MainActivity4.java
+++ app/src/main/java/com/example/project_belgie/MainActivity4.java
@@ -11,6 +11,11 @@ public class MainActivity4 extends AppCompatActivity {
 
     //init textviews
     TextView textView1;
+    TextView textView3;
+    TextView textView4;
+    TextView textView5;
+    TextView textView6;
+    TextView textView7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +24,17 @@ public class MainActivity4 extends AppCompatActivity {
 
         //conect de textviews
         textView1 = findViewById(R.id.text1);
+        textView3 = findViewById(R.id.text3);
+        textView4 = findViewById(R.id.text4);
+        textView5 = findViewById(R.id.text5);
+        textView6 = findViewById(R.id.text6);
+        textView7 = findViewById(R.id.text7);
 
         //functies
-        click_1();
+        click_back_1();
     }
-
-    public void click_1(){
+    //click listner wanneer er op de back word geduwd
+    public void click_back_1(){
         textView1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,4 +43,5 @@ public class MainActivity4 extends AppCompatActivity {
             }
         });
     }
+
 }

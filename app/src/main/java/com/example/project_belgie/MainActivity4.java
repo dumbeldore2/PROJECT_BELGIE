39eb8fab9394c39265d6996f8b5ac00eb0640ba4
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-07-08 22:25:51 +0200

    MainActivity4.java heeft een change color fun

--- app/src/main/java/com/example/project_belgie/MainActivity4.java
+++ app/src/main/java/com/example/project_belgie/MainActivity4.java
@@ -3,6 +3,7 @@ package com.example.project_belgie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -46,6 +47,19 @@ public class MainActivity4 extends AppCompatActivity {
 
         //functies
         click_back_1();
+        click_3();
+        click_4();
+        click_5();
+        click_6();
+        click_7();
+
+
+        //default color changes voor d textviews
+        color_changer_fun(textView3,0);
+        color_changer_fun(textView4,1);
+        color_changer_fun(textView5,2);
+        color_changer_fun(textView6,3);
+        color_changer_fun(textView7,4);
     }
     //click listner wanneer er op de back word geduwd
     public void click_back_1(){
@@ -143,5 +157,14 @@ public class MainActivity4 extends AppCompatActivity {
                 database.setTable_3_col_1_tot_5(arrayList);
             }
         });
+    }
+
+    //de color changes
+    public void color_changer_fun(TextView textView , int id){
+        if (arrayList.get(id).equals("true")){
+            textView.setBackgroundResource(R.drawable.border_3);
+        } else {
+            textView.setBackgroundColor(Color.parseColor("#C4C4C4"));
+        }
     }
 }

cb898481451032af8233cbb83e5b13565e8ace46
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-07-08 20:43:58 +0200

    border2 is aangemaakt en MainActivity3.java is aangepast zodat deze deze soort van kan gebvruiken

--- app/src/main/java/com/example/project_belgie/MainActivity3.java
+++ app/src/main/java/com/example/project_belgie/MainActivity3.java
@@ -3,6 +3,7 @@ package com.example.project_belgie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -17,6 +18,13 @@ public class MainActivity3 extends AppCompatActivity {
 
     //textviews initen
     TextView textView1;
+    TextView textView3;
+    TextView textView4;
+    TextView textView5;
+    TextView textView6;
+    TextView textView7;
+    TextView textView8;
+    TextView textView9;
 
     //arraylist
     ArrayList<String> arrayList;
@@ -34,6 +42,13 @@ public class MainActivity3 extends AppCompatActivity {
 
         // texvieuws conecten
         textView1 = findViewById(R.id.text1);
+        textView3 = findViewById(R.id.text3);
+        textView4 = findViewById(R.id.text4);
+        textView5 = findViewById(R.id.text5);
+        textView6 = findViewById(R.id.text6);
+        textView7 = findViewById(R.id.text7);
+        textView8 = findViewById(R.id.text8);
+        textView9 = findViewById(R.id.text9);
 
         //database conecten
         database = new Database(this);
@@ -48,22 +63,103 @@ public class MainActivity3 extends AppCompatActivity {
 
 
         //functies
-        click_1();
         click_back_1();
+        click_5();
+        click_6();
+        click_7();
+        click_8();
+        click_9();
         sout_fun();
         sout_fun2();
+
+
+        //default color changes voor d textviews
+        color_changer_fun(textView5,0);
+        color_changer_fun(textView6,1);
+        color_changer_fun(textView7,2);
+        color_changer_fun(textView8,3);
+        color_changer_fun(textView9,4);
     }
 
-    public void click_1(){
-        textView1.setOnClickListener(new View.OnClickListener() {
+    public void click_5(){
+        textView5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(intent);
+                if (arrayList1.get(0).equals("false")){
+                    arrayList1.set(0,"true");
+                    color_changer_fun(textView4,0);
+                }   else    {
+                    arrayList1.set(0,"false");
+                    color_changer_fun(textView4,0);
+                }
+                database.setTable_2_col_1_tot_8(arrayList, arrayList1);
             }
         });
     }
 
+    public void click_6(){
+        textView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (arrayList1.get(1).equals("false")){
+                    arrayList1.set(1,"true");
+                    color_changer_fun(textView4,1);
+                }   else    {
+                    arrayList1.set(1,"false");
+                    color_changer_fun(textView4,1);
+                }
+                database.setTable_2_col_1_tot_8(arrayList, arrayList1);
+            }
+        });
+    }
+
+    public void click_7(){
+        textView7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (arrayList1.get(2).equals("false")){
+                    arrayList1.set(2,"true");
+                    color_changer_fun(textView4,2);
+                }   else    {
+                    arrayList1.set(2,"false");
+                    color_changer_fun(textView4,2);
+                }
+                database.setTable_2_col_1_tot_8(arrayList, arrayList1);
+            }
+        });
+    }
+
+    public void click_8(){
+        textView8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (arrayList1.get(3).equals("false")){
+                    arrayList1.set(3,"true");
+                    color_changer_fun(textView4,3);
+                }   else    {
+                    arrayList1.set(3,"false");
+                    color_changer_fun(textView4,3);
+                }
+                database.setTable_2_col_1_tot_8(arrayList, arrayList1);
+            }
+        });
+    }
+
+    public void click_9(){
+        textView9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (arrayList1.get(4).equals("false")){
+                    arrayList1.set(4,"true");
+                    color_changer_fun(textView4,4);
+                }   else    {
+                    arrayList1.set(4,"false");
+                    color_changer_fun(textView4,4);
+                }
+                database.setTable_2_col_1_tot_8(arrayList, arrayList1);
+            }
+        });
+    }
     /*public void change_naam(){
         if(!database.getTable_1_col_1().isEmpty() && database.getTable_1_col_1() != null && !database.getTable_1_col_1().equals("null")){
             editText1.setText(database.getTable_1_col_1());
@@ -116,6 +212,16 @@ public class MainActivity3 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+
+    //de color changes
+    public void color_changer_fun(TextView textView , int id){
+        if (arrayList1.get(id).equals("true")){
+            textView.setBackgroundResource(R.drawable.border_2);
+        } else {
+            textView.setBackgroundColor(Color.parseColor("#C4C4C4"));
+        }
     }
 
 }

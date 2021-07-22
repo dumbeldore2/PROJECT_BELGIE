2b3afa33359fc3307ff83939c432b86fa2bfde8e
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2021-07-22 20:56:26 +0200

    MainActivity13.java zou moeten werken

--- app/src/main/java/com/example/project_belgie/MainActivity13.java
+++ app/src/main/java/com/example/project_belgie/MainActivity13.java
@@ -3,15 +3,26 @@ package com.example.project_belgie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity13 extends AppCompatActivity {
 
     //initen van textviews
     TextView textView1;
+    TextView textView3;
+    TextView textView4;
+    TextView textView5;
 
+    //arraylist
+    ArrayList<String> arrayList;
+
+    //database initen
+    Database database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,18 +30,99 @@ public class MainActivity13 extends AppCompatActivity {
 
         //conecten van textviews
         textView1 = findViewById(R.id.text1);
+        textView3 = findViewById(R.id.text3);
+        textView4 = findViewById(R.id.text4);
+        textView5 = findViewById(R.id.text5);
+
+        //database conecten
+        database = new Database(this);
+
+        //araylisten conecten
+        arrayList = database.Table_12_col_1_tot_3();
+
+        //default color changes voor d textviews
+        color_changer_fun(textView3,0);
+        color_changer_fun(textView4,1);
+        color_changer_fun(textView5,2);
 
         //functies
-        click_1();
+        click_back_1();
+        //sout_fun();
+        click_3();
+        click_4();
+        click_5();
     }
 
-    public void click_1(){
+    //click listner wanneer er op de back word geduwd
+    public void click_back_1(){
         textView1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    //sout functies voor beide arrays
+    public void sout_fun(){
+        for (int i = 0 ; i < arrayList.size() ; i++){
+            System.out.println(arrayList.get(i));
+        }
+    }
+
+    //alle click functies
+    public void click_3(){
+        textView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (arrayList.get(0).equals("false")){
+                    arrayList.set(0,"true");
+                    color_changer_fun(textView3,0);
+                }   else    {
+                    arrayList.set(0,"false");
+                    color_changer_fun(textView3,0);
+                }
+                database.setTable_11_col_1_tot_12(arrayList);
+            }
+        });
+    }  public void click_4(){
+        textView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (arrayList.get(1).equals("false")){
+                    arrayList.set(1,"true");
+                    color_changer_fun(textView4,1);
+                }   else    {
+                    arrayList.set(1,"false");
+                    color_changer_fun(textView4,1);
+                }
+                database.setTable_11_col_1_tot_12(arrayList);
+            }
+        });
+    }
+    public void click_5(){
+        textView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (arrayList.get(2).equals("false")){
+                    arrayList.set(2,"true");
+                    color_changer_fun(textView5,2);
+                }   else    {
+                    arrayList.set(2,"false");
+                    color_changer_fun(textView5,2);
+                }
+                database.setTable_11_col_1_tot_12(arrayList);
+            }
+        });
+    }
+
+    //de color changes
+    public void color_changer_fun(TextView textView , int id){
+        if (arrayList.get(id).equals("true")){
+            textView.setBackgroundResource(R.drawable.border_12);
+        } else {
+            textView.setBackgroundColor(Color.parseColor("#C4C4C4"));
+        }
     }
 }
